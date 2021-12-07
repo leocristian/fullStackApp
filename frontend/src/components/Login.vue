1e1df72099404413e746255aa2a565aa92b71bbe
leocristian <leonardosclopes@gmail.com>
2021-12-06 21:18:44 -0300

    add frontend

---
+++ frontend/src/components/Login.vue
@@ -0,0 +1,16 @@
+<template>
+    <form action="#">
+        <h3>Nome de usuário</h3>
+        <input type="text">
+        <h3>Senha</h3>
+        <input type="password">   
+    </form>    
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+</style>
